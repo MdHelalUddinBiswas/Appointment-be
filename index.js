c33c5bfde23db3bb75ccf06add1b1cf588e1a09b
MdHelalUddinBiswas <helal@nexstack.sg>
2025-05-17 14:16:07 +0600

    update jwt

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const cors = require("cors");
 const { Pool } = require("pg");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 require("dotenv").config();
 
 const app = express();
